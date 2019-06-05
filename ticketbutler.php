5d7aaba3c23b759a924b2f628e3eae8418d616f3
svnf <santosh@worksonline.dk>
2019-06-05 15:14:21 +0530

    Tickerbutler config page

--- ticketbutler.php
+++ ticketbutler.php
@@ -1,9 +1,28 @@
 <?php
 /**
+ * 2007-2018 PrestaShop
  *
- * @author    SchledermannCo <jo@schledermannco.com>
- * @copyright 2007-2019 SchledermannCo
- * @license   https://www.gnu.org/licenses/gpl-3.0.en.html General Public License (GPL 3.0)
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/OSL-3.0
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+ * versions in the future. If you wish to customize PrestaShop for your
+ * needs please refer to http://www.prestashop.com for more information.
+ *
+ * @author    PrestaShop SA <contact@prestashop.com>
+ * @copyright 2007-2018 PrestaShop SA
+ * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
+ * International Registered Trademark & Property of PrestaShop SA
+ 
  * @author    San & Johan
  *  
  */
@@ -14,10 +33,10 @@ if (!defined('_PS_VERSION_')) {
 
 class TicketButler extends Module {
 
-	public function __construct() {
-		$this->name = 'ticketbutler';
+    public function __construct() {
+        $this->name = 'ticketbutler';
         $this->tab = 'administration';
-        $this->author = 'SchledermannCo';
+        $this->author = 'Johan';
         $this->version = '1.0';
         $this->need_instance = 0;
         $this->bootstrap = true;
@@ -34,7 +53,10 @@ class TicketButler extends Module {
         if (!parent::install() || !$this->_installSql()
                 //Register hooks here
                 || ! $this->registerHook('actionPaymentConfirmation')
-                || ! $this->registerHook('displayAdminProductsExtra')        
+                || ! $this->registerHook('displayAdminProductsExtra')
+|| !Configuration::updateValue('ticketbutler_api', 'api')
+                || !Configuration::updateValue('ticketbutler_token', 'token')
+                
         ) {
             return false;
         }
@@ -46,7 +68,9 @@ class TicketButler extends Module {
  
     public function uninstall() { 
  
-        return parent::uninstall() && $this->_unInstallSql() && $this->unregisterHook('displayAdminProductsExtra') && $this->unregisterHook('ActionPaymentConfirmation');
+        return parent::uninstall() && $this->_unInstallSql() &&  
+        !Configuration::deleteByName('ticketbutler_api') &&  
+        !Configuration::deleteByName('ticketbutler_token') && $this->unregisterHook('displayAdminProductsExtra') && $this->unregisterHook('ActionPaymentConfirmation');
     }
  
     /**
@@ -104,4 +128,99 @@ class TicketButler extends Module {
            );
         return $this->display(__FILE__, 'views/templates/hook/ticketbutler.tpl');
     }
+  
+    public function getContent()
+{
+    $output = null;
+
+    if (Tools::isSubmit('submit'.$this->name)) {
+        $ticketbutlerApiEndpoint = strval(Tools::getValue('ticketbutler_api'));
+         $ticketbutlerApiToken = strval(Tools::getValue('ticketbutler_token'));
+
+        if (
+            !$ticketbutlerApiEndpoint ||
+            empty($ticketbutlerApiEndpoint) ||
+            !Validate::isGenericName($ticketbutlerApiEndpoint) || !$ticketbutlerApiToken ||
+            empty($ticketbutlerApiToken) ||
+            !Validate::isGenericName($ticketbutlerApiToken)
+        ) {
+            $output .= $this->displayError($this->l('Invalid Configuration value'));
+        } else {
+            Configuration::updateValue('ticketbutler_api', $ticketbutlerApiEndpoint);
+            Configuration::updateValue('ticketbutler_token', $ticketbutlerApiToken);
+            $output .= $this->displayConfirmation($this->l('Settings updated'));
+        }
+    }
+
+    return $output.$this->displayForm();
+}
+
+
+
+public function displayForm()
+{
+    
+    $defaultLanguage = (int)Configuration::get('PS_LANG_DEFAULT');
+ 
+    $fieldsForm[0]['form'] = [
+        'legend' => [
+            'title' => $this->l('Ticketbutler API Configuration'),
+        ],
+        'input' => [
+            [
+                'type' => 'text',
+                'label' => $this->l('Ticketbutler API Endpoint'),
+                'name' => 'ticketbutler_api',
+                'size' => 20,
+                'required' => true
+            ],
+        [
+                'type' => 'text',
+                'label' => $this->l('Ticketbutler API Token'),
+                'name' => 'ticketbutler_token',
+                'size' => 20,
+                'required' => true
+            ]
+        ],
+         'submit' => [
+            'title' => $this->l('Save'),
+            'class' => 'btn btn-default pull-right'
+        ]  
+ 
+    ];
+ 
+
+
+    $helper = new HelperForm();
+ 
+    $helper->module = $this;
+    $helper->name_controller = $this->name;
+    $helper->token = Tools::getAdminTokenLite('AdminModules');
+    $helper->currentIndex = AdminController::$currentIndex.'&configure='.$this->name;
+ 
+    $helper->default_form_language = $defaultLanguage;
+    $helper->allow_employee_form_lang = $defaultLanguage;
+ 
+    $helper->title = $this->displayName;
+    $helper->show_toolbar = true;        // false -> remove toolbar
+    $helper->toolbar_scroll = true;      // yes - > Toolbar is always visible on the top of the screen.
+    $helper->submit_action = 'submit'.$this->name;
+    $helper->toolbar_btn = [
+        'save' => [
+            'desc' => $this->l('Save'),
+            'href' => AdminController::$currentIndex.'&configure='.$this->name.'&save'.$this->name.
+            '&token='.Tools::getAdminTokenLite('AdminModules'),
+        ],
+        'back' => [
+            'href' => AdminController::$currentIndex.'&token='.Tools::getAdminTokenLite('AdminModules'),
+            'desc' => $this->l('Back to list')
+        ]
+    ];
+ 
+    $helper->fields_value['ticketbutler_api'] = Configuration::get('ticketbutler_api');
+   $helper->fields_value['ticketbutler_token'] = Configuration::get('ticketbutler_token');
+
+    return $helper->generateForm($fieldsForm);
+}
+
 }
